3f6a771c126d882c368a04f962b70f4e7ec71ec3
Kamesas <semakaleksandr2014@gmail.com>
2018-09-01 17:00:25 +0300

    propTypes for func component

--- src/Comments.jsx
+++ src/Comments.jsx
@@ -12,3 +12,24 @@ class Comments extends Component {
 }
 
 export default Comments;
+
+// import React from 'react'
+// import PropTypes from 'prop-types'
+
+// function Comments({comment}) {
+//     return (
+//         <div>
+//         <h5>{this.props.user}</h5>
+//         <p>{this.props.text}</p>
+//         </div>
+//     )
+// }
+
+// Comment.propTypes = {
+//     comment: PropTypes.shape({
+//         text: PropTypes.string.isRequired,
+//         user: PropTypes.string.isRequired
+//     }).isRequired
+// }
+
+// export default Comments
