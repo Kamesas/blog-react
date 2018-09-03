e852a56747766cf23946989c286c9fac23110514
Kamesas <semakaleksandr2014@gmail.com>
2018-09-03 15:06:50 +0300

    some change

--- src/decorators/accardeon.jsx
+++ src/decorators/accardeon.jsx
@@ -1,7 +1,7 @@
 import React, { Component as ReactComponent } from "react";
 
 export default OriginalComponent =>
-  class WrappedComponent extends ReactComponent {
+  class WrappAccardeon extends ReactComponent {
     state = {
       openArticleId: null
     };
