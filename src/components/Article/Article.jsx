0a99b90caef0a2b937607990d647dac20939f8ec
Kamesas <semakaleksandr2014@gmail.com>
2018-09-07 14:46:35 +0300

    changed folder structure

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import CommentsList from "./CommentsList";
-import CommentForm from "./CommentForm/CommentForm";
+import CommentsList from "../CommentsList";
+import CommentForm from "../CommentForm/CommentForm";
 import { CSSTransitionGroup } from "react-transition-group";
 import "./article.css";
 
@@ -34,7 +34,7 @@ class Article extends Component {
           transitionName="article"
           transitionAppear
           transitionEnterTimeout={300}
-          transitionLeaveTimeout={500}
+          transitionLeaveTimeout={300}
           transitionAppearTimeout={500}
           component="div"
         >
